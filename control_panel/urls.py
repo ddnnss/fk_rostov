12a0712f1d71346fcfa10db0a8d52383bf9def8c
ddnnss <ddnnss.i1@gmail.com>
2020-06-16 23:27:20 +0300

    add sector tickets price adding

--- control_panel/urls.py
+++ control_panel/urls.py
@@ -4,6 +4,8 @@ from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('show_sector/<match_id>/<sector_slug>', views.show_sector, name='show_sector'),
+    path('get_sector/', views.get_sector, name='get_sector'),
+    path('save_sector/', views.save_sector, name='save_sector'),
 
 
 
--- control_panel/views.py
+++ control_panel/views.py
@@ -1,16 +1,27 @@
+import json
+from django.http import JsonResponse
 from django.shortcuts import render
 from stadium.models import *
 from match.models import *
 def show_sector(request,match_id,sector_slug):
-    match = Match.objects.get(id=match_id)
-    sector = Sector.objects.get(name_slug=sector_slug)
+    match = Match.objects.get(id=match_id).id
+    sector = Sector.objects.get(name_slug=sector_slug).name_slug
+    sector_name = Sector.objects.get(name_slug=sector_slug).name
+    return render(request, 'cp/show_sector.html', locals())
+
+def get_sector(request):
+    body = json.loads(request.body)
+
+    print(body)
+    match = Match.objects.get(id=body['match'])
+    sector = Sector.objects.get(name_slug=body['sector'])
     # print(sector)
     rows = Row.objects.filter(sector=sector)
     # print(rows)
-    sector_map=[]
+    sector_map = []
 
     for row in rows:
-        row_map=[]
+        row_map = []
         seats = Place.objects.filter(row=row)
         for seat in seats:
             # print(seats)
@@ -19,8 +30,32 @@ def show_sector(request,match_id,sector_slug):
             except:
                 price = 0
 
-            row_map.append({'row': row.number,'seat':seat.number,'price':price, 'selected':0})
+            row_map.append({'row': row.number, 'seat': seat.number, 'price': price, 'selected': 0})
         sector_map.append(row_map)
     print(sector_map)
-    return render(request, 'cp/show_sector.html', locals())
+    return JsonResponse(sector_map,safe=False)
 
+def save_sector(request):
+    body = json.loads(request.body)
+    sector_slug = body['sector']
+    match_id = body['match']
+    rows = body['rows']
+
+    for row in rows:
+        # print (len(row))
+        for i in range(0,len(row)):
+            # print(row[i])
+            cur_row=int(row[i]['row'])
+            cur_seat=int(row[i]['seat'])
+            cur_price=int(row[i]['price'])
+            # if cur_price > 0:
+            tempSector = Sector.objects.get(name_slug=sector_slug)
+            tempRow = Row.objects.get(sector=tempSector,number=cur_row)
+            tempSeat = Place.objects.get(row=tempRow,number=cur_seat)
+            tempPrice,created = Price.objects.get_or_create(match_id=match_id,place=tempSeat,defaults={'price':cur_price})
+            if not created:
+                print('price found')
+                tempPrice.price = cur_price
+                tempPrice.save()
+
+    return JsonResponse({'result':'ok'}, safe=False)
